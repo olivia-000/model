5325333a46bb55bacdbe29a9d6e207c918359b5f
olivia-000 <olivia0966305653@gmail.com>
2025-10-02 16:02:46 +0800

    Create Q3_3.aspx.cs

---
+++ C#網頁設計/HW10/Q3_3.aspx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Q3_3 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnShow_Click(object sender, EventArgs e)
+    {
+        if (Page.IsValid)
+        {
+            Session["Hobby"] = ddlHobby.SelectedValue;
+
+            lblAll.Text = $@"
+            帳號：{Session["User"]}<br/>
+            密碼：{Session["Pwd"]}<br/>
+            地址：{Session["Addr"]}<br/>
+            電話：{Session["Tel"]}<br/>
+            生日：{Session["Birth"]}<br/>
+            興趣：{Session["Hobby"]}";
+        }
+    }
+}
