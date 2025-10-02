d269863b19c470133615fce2684374717bd5c8aa
olivia-000 <olivia0966305653@gmail.com>
2025-10-02 16:08:35 +0800

    Delete StudentRepository.java

--- B1144257_hw0312/hw0312/src/main/java/cgu/library/app1/domain/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package cgu.library.app1.domain;
-
-import org.springframework.data.repository.CrudRepository;
-import java.util.List;
-
-public interface StudentRepository extends CrudRepository<Student, Long> {
-    // 自定義查詢方法：搜尋上半年（1~6月）出生的學生
-    List<Student> findByBirthMonthBetween(int start, int end);
-}
