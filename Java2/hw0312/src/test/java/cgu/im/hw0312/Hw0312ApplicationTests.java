e656b67f86e24cb5afa175d72958ae54b26a1ba6
olivia-000 <olivia0966305653@gmail.com>
2025-10-02 16:07:50 +0800

    Create Hw0312ApplicationTests.java

---
+++ Java2/hw0312/src/test/java/cgu/im/hw0312/Hw0312ApplicationTests.java
@@ -0,0 +1,13 @@
+package cgu.im.hw0312;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw0312ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
