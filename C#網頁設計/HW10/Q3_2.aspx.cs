0d5a80faf6f2908f7da3afd17d58eef9a9a24eec
olivia-000 <olivia0966305653@gmail.com>
2025-10-02 16:02:42 +0800

    Create Q3_2.aspx.cs

---
+++ C#網頁設計/HW10/Q3_2.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Q3_2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnNext_Click(object sender, EventArgs e)
+    {
+        if (Page.IsValid)
+        {
+            Session["Addr"] = txtAddr.Text;
+            Session["Tel"] = txtTel.Text;
+            Session["Birth"] = txtBirth.Text;
+            Response.Redirect("Q3_3.aspx");
+        }
+    }
+}
