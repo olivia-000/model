320a933c8d2dd5a6f6635d002aab434332c855bc
olivia-000 <olivia0966305653@gmail.com>
2025-10-02 16:02:34 +0800

    Create Q2_2.aspx.cs

---
+++ C#網頁設計/HW10/Q2_2.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Q2_2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        HttpCookie ck = Request.Cookies["Q2"];
+        if (ck == null)
+        {
+            lblResult.Text = "Cookie 遺失，請重新輸入。";
+            return;
+        }
+
+        string name = ck["Name"];
+        int principal = int.Parse(ck["Principal"]);
+        double rate = double.Parse(ck["Rate"]);
+        int years = int.Parse(ck["Years"]);
+
+        double amount = principal;
+        for (int i = 0; i < years; i++)
+            amount *= (1 + rate);
+
+        int total = (int)Math.Floor(amount); // 無條件捨去
+
+        lblResult.Text =
+            $"{name}您好，你儲存的本金 {principal} 元，{years} 年後可領回 {total} 元。";
+    }
+
+
+}
