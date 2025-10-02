71ee7e87236e9ba8c6bcd089ab593969f5bf0e89
olivia-000 <olivia0966305653@gmail.com>
2025-10-02 16:02:21 +0800

    Create Q1_2.aspx.cs

---
+++ C#網頁設計/HW10/Q1_2.aspx.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Q1_2 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lblResult.Text = Session["Q1Msg"]?.ToString() ?? "Session 遺失，請重新計算。";
+    }
+
+}
